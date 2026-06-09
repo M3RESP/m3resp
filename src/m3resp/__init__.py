34d8cfaea149303fcaae4fe16cc91b5bbc028ecc
mahyar-t <mahyar.taherpour@gmail.com>
2026-06-09 10:53:44 +0200

    feat: add associated configuration helpers in the public API

--- src/m3resp/__init__.py
+++ src/m3resp/__init__.py
@@ -1,5 +1,6 @@
 """M3Resp public API."""
 
+from m3resp.core.config import WorkflowConfig, load_workflow_config
 from m3resp.core.events import BreathEvent, Event
 from m3resp.core.session import M3Session
 from m3resp.modalities.eit import load as load_eit
@@ -7,4 +8,12 @@ from m3resp.modalities.emg import load as load_emg
 
 __version__ = "0.1.0"
 
-__all__ = ["BreathEvent", "Event", "M3Session", "load_eit", "load_emg"]
+__all__ = [
+    "BreathEvent",
+    "Event",
+    "M3Session",
+    "WorkflowConfig",
+    "load_eit",
+    "load_emg",
+    "load_workflow_config",
+]
